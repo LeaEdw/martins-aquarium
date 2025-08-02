082fac84504e0cd4e36cd55931af7850526a5b45
Lea Edwards <leaxedw@gmail.com>
2025-08-02 09:42:13 -0500

    Fixed some CSS, added more fish objects, added new module for multiples

--- scripts/aquariumData.js
+++ scripts/aquariumData.js
@@ -6,7 +6,8 @@ export const database = {
       length: 3,
       location: "the Great Barrier Reef",
       diet: "omnivore",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Amphiprion_ocellaris_%28Clown_anemonefish%29_by_Nick_Hobgood.jpg/1200px-Amphiprion_ocellaris_%28Clown_anemonefish%29_by_Nick_Hobgood.jpg",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Amphiprion_ocellaris_%28Clown_anemonefish%29_by_Nick_Hobgood.jpg/1200px-Amphiprion_ocellaris_%28Clown_anemonefish%29_by_Nick_Hobgood.jpg",
     },
     {
       name: "Dory",
@@ -14,7 +15,8 @@ export const database = {
       length: 5,
       location: "the Great Barrier Reef",
       diet: "omnivore",
-      image: "https://upload.wikimedia.org/wikipedia/commons/1/13/Paletten-Doktorfisch_M%C3%BCnster.JPG",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/1/13/Paletten-Doktorfisch_M%C3%BCnster.JPG",
     },
     {
       name: "Darcy",
@@ -31,7 +33,8 @@ export const database = {
       length: 55,
       location: "Freshwater",
       diet: "omnivore",
-      image: "https://animals.sandiegozoo.org/sites/default/files/2016-11/Koi.jpg",
+      image:
+        "https://animals.sandiegozoo.org/sites/default/files/2016-11/Koi.jpg",
     },
     {
       name: "Toni",
@@ -61,13 +64,149 @@ export const database = {
         "https://www.4ocean.com/cdn/shop/articles/shutterstock_598583180.jpg?v=1707269243&width=1600",
     },
     {
-        name: "Jack",
-        species: "Anglerfish",
-        length: 16,
-        location: "Abyssal Plains",
-        diet: "carnivore",
-        image: "https://ask-nature.sfo3.digitaloceanspaces.com/wp-content/uploads/2011/06/31142638/Anglerfish.jpg",
-    }
+      name: "Jack",
+      species: "Anglerfish",
+      length: 16,
+      location: "Abyssal Plains",
+      diet: "carnivore",
+      image:
+        "https://ask-nature.sfo3.digitaloceanspaces.com/wp-content/uploads/2011/06/31142638/Anglerfish.jpg",
+    },
+    {
+      name: "Marina",
+      species: "Goldfish",
+      length: 12, // divisible by 3
+      location: "Freshwater",
+      diet: "omnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Goldfish3.jpg/1200px-Goldfish3.jpg",
+    },
+    {
+      name: "Rex",
+      species: "Great White Shark",
+      length: 150, // divisible by 3 and 5
+      location: "Pacific Ocean",
+      diet: "carnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/White_shark.jpg/1200px-White_shark.jpg",
+    },
+    {
+      name: "Luna",
+      species: "Manta Ray",
+      length: 200, // divisible by 5
+      location: "Tropical Oceans",
+      diet: "planktivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Manta_birostris-Thailand4.jpg/1200px-Manta_birostris-Thailand4.jpg",
+    },
+    {
+      name: "Oscar",
+      species: "Oscar Cichlid",
+      length: 36, // divisible by 3
+      location: "Amazon River",
+      diet: "omnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Astronotus_ocellatus.jpg/1200px-Astronotus_ocellatus.jpg",
+    },
+    {
+      name: "Zippy",
+      species: "Yellowfin Tuna",
+      length: 75, // divisible by 3 and 5
+      location: "Atlantic Ocean",
+      diet: "carnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Yellowfin-tuna.jpg/1200px-Yellowfin-tuna.jpg",
+    },
+    {
+      name: "Patches",
+      species: "Pufferfish",
+      length: 25, // divisible by 5
+      location: "Coral Reefs",
+      diet: "omnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Takifugu_rubripes.jpg/1200px-Takifugu_rubripes.jpg",
+    },
+    {
+      name: "Finn",
+      species: "Northern Pike",
+      length: 48, // divisible by 3
+      location: "Northern Lakes",
+      diet: "carnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Esox_lucius1.jpg/1200px-Esox_lucius1.jpg",
+    },
+    {
+      name: "Coral",
+      species: "Mandarin Fish",
+      length: 8,
+      location: "Pacific Coral Reefs",
+      diet: "carnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Synchiropus_splendidus_2_Luc_Viatour.jpg/1200px-Synchiropus_splendidus_2_Luc_Viatour.jpg",
+    },
+    {
+      name: "Storm",
+      species: "Electric Eel",
+      length: 90, // divisible by 3 and 5
+      location: "Amazon Basin",
+      diet: "carnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Electric_eel.jpg/1200px-Electric_eel.jpg",
+    },
+    {
+      name: "Sunny",
+      species: "Sunfish",
+      length: 40, // divisible by 5
+      location: "Temperate Oceans",
+      diet: "omnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Mola_mola.png/1200px-Mola_mola.png",
+    },
+    {
+      name: "Blaze",
+      species: "Firefish",
+      length: 9, // divisible by 3
+      location: "Indo-Pacific",
+      diet: "planktivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Nemateleotris_magnifica.jpg/1200px-Nemateleotris_magnifica.jpg",
+    },
+    {
+      name: "Archer",
+      species: "Archerfish",
+      length: 20, // divisible by 5
+      location: "Mangrove Swamps",
+      diet: "carnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Toxotes_jaculatrix.jpg/1200px-Toxotes_jaculatrix.jpg",
+    },
+    {
+      name: "Ruby",
+      species: "Red Snapper",
+      length: 42, // divisible by 3
+      location: "Gulf of Mexico",
+      diet: "carnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Red_snapper.jpg/1200px-Red_snapper.jpg",
+    },
+    {
+      name: "Mystic",
+      species: "Moray Eel",
+      length: 85, // divisible by 5
+      location: "Tropical Reefs",
+      diet: "carnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Gymnothorax_javanicus_2.jpg/1200px-Gymnothorax_javanicus_2.jpg",
+    },
+    {
+      name: "Splash",
+      species: "Rainbow Trout",
+      length: 27, // divisible by 3
+      location: "Mountain Streams",
+      diet: "omnivore",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Oncorhynchus_mykiss.jpg/1200px-Oncorhynchus_mykiss.jpg",
+    },
   ],
   tips: [
     {
@@ -89,34 +228,39 @@ export const database = {
   ],
   locations: [
     {
-        name: "Great Barrier Reef",
-        country: "Australia",
-        description: "The Great Barrier Reef is the world's largest coral reef system, supporting a diverse ecosystem of marine life."
+      name: "Great Barrier Reef",
+      country: "Australia",
+      description:
+        "The Great Barrier Reef is the world's largest coral reef system, supporting a diverse ecosystem of marine life.",
     },
     {
-        name: "Freshwater Rivers",
-        country: "Canada",
-        description: "The Mackenzie River is the longest river in Canada."
+      name: "Freshwater Rivers",
+      country: "Canada",
+      description: "The Mackenzie River is the longest river in Canada.",
     },
     {
-        name: "Freshwater Ponds",
-        country: "Japan",
-        description: "Freshwater ponds are bodies of standing fresh waters. They are smaller and shallower than lakes and support diverse plant and animal life."
+      name: "Freshwater Ponds",
+      country: "Japan",
+      description:
+        "Freshwater ponds are bodies of standing fresh waters. They are smaller and shallower than lakes and support diverse plant and animal life.",
     },
     {
-        name: "Saltwater Lakes",
-        country: "Palestine",
-        description: "The Dead Sea is one of the world's saltiest bodies of water with a salinity of 34.2% - it is 9 times saltier than the ocean!"
+      name: "Saltwater Lakes",
+      country: "Palestine",
+      description:
+        "The Dead Sea is one of the world's saltiest bodies of water with a salinity of 34.2% - it is 9 times saltier than the ocean!",
     },
     {
-        name: "Kelimutu Crater Lakes",
-        country: "Indonesia",
-        description: "These lakes exists in craters of the Kelimutu volcano and exhibit a unique phenomenon of changing colors."
+      name: "Kelimutu Crater Lakes",
+      country: "Indonesia",
+      description:
+        "These lakes exists in craters of the Kelimutu volcano and exhibit a unique phenomenon of changing colors.",
     },
     {
-        name: "Lake Baikal",
-        country: "Russia",
-        description: "Estimated to be 25 million years old with a depth of 5387 feet - its is the oldest and deepest lake in the world."
-    }
-  ]
+      name: "Lake Baikal",
+      country: "Russia",
+      description:
+        "Estimated to be 25 million years old with a depth of 5387 feet - its is the oldest and deepest lake in the world.",
+    },
+  ],
 };
